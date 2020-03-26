60e72830ee6f222befd0afa765598ee03e80fec1
Nils Bertschinger <bertschi@casino.fias.uni-frankfurt.de>
2020-03-26 01:28:28 +0100

    deeper analysis of option implied crash risk and started write-up

--- options.R
+++ options.R
@@ -3,7 +3,13 @@
 library(tidyverse)
 library(ggthemes)
 library(quantmod)
+library(lubridate)
+library(tidybayes)
 library(rstan)
+library(loo)
+
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
 
 sp500_opts <- getOptionChain("^GSPC", NULL)
 
@@ -110,15 +116,119 @@ model <- stan_model(model_code = code)
 data <- df_opts %>%
     mutate(Price = (Ask + Bid) / 2,
            Maturity = Expiration - today()) %>%
-    filter(Vol > 100) %>%
-    filter(Expiration == parse_date("Dec.17.2021", "%b.%d.%Y"))
+    filter(OI > 100 & Vol > 100)
 
-fit <- sampling(model,
-                data = list(N = nrow(data),
-                            prices = data$Price,
-                            strikes = data$Strike,
-                            callput = (data$Type == "Call"),
-                            K = 2,
-                            tau = as.numeric(unique(data$Maturity)),
-                            r = 0.0),
-                chains = 2)
+r <- 0.0
+
+fits <- data %>%
+    crossing(K = 1:3) %>%
+    group_by(Expiration, Maturity, K) %>%
+    nest() %>%
+    mutate(fits = pmap(list(data, Maturity, K),
+                      function (data, tau, K)
+                          sampling(model,
+                                   data = list(N = nrow(data),
+                                               prices = data$Price,
+                                               strikes = data$Strike,
+                                               callput = (data$Type == "Call"),
+                                               K = K,
+                                               tau = as.numeric(unique(tau)),
+                                               r = r),
+                                   chains = 3))) %>%
+    ungroup()
+
+## Visual model comparison based on LOO
+fits %>%
+    mutate(log_lik = map(fits,
+                         ~ loo(.x)$pointwise %>% as_tibble())) %>%
+    unnest(log_lik) %>%
+    ggplot(aes(elpd_loo,
+               fill = factor(K))) +
+    geom_density(alpha = 0.4) +
+    facet_grid(Expiration ~ .) +
+    scale_fill_colorblind() +
+    theme_tufte() +
+    theme(text = element_text(size = 12),
+          legend.position = "top") +
+    labs(x = "LOO log likelihood",
+         y = NULL,
+         fill = "K",
+         title = "S&P 500",
+         subtitle = paste("Option chain from", today(),
+                          sep = " "))
+
+ggsave("reports/model_loo.pdf")
+
+## Compute spot price from put-call parity
+spots <- data %>%
+    select(Expiration, Strike, Type, Price, Maturity) %>%
+    group_by(Expiration) %>%
+    spread(Type, Price) %>%
+    drop_na() %>%
+    mutate(Spot = Call - Put + exp(- r * as.numeric(Maturity)) * Strike) %>%
+    ungroup()
+
+pdfs <- fits %>%
+    left_join(spots %>%
+              group_by(Expiration) %>%
+              summarize(Spot = median(Spot)) %>%
+              ungroup()) %>%
+    mutate(pdfs = pmap(list(fits, Spot, Maturity),
+                      function (fit, spot, tau) {
+                          fit %>%
+                              spread_draws(F[k], sigma[k], theta[k]) %>%
+                              group_by(.chain, .iteration, .draw, k) %>%
+                              mutate(pdf = pmap(list(theta, F, sigma),
+                                                function (theta, F, sigma)
+                                                    tibble(S = seq(spot / 3, spot * 2,
+                                                                   length.out = 101)) %>%
+                                                    mutate(pdf = theta * dlnorm(S, log(F), sqrt(as.numeric(tau)) * sigma)))) %>%
+                              ungroup() %>%
+                              unnest(pdf)
+                      })) %>%
+    unnest(pdfs)
+
+pdfs %>%
+    filter(K == 2) %>%
+    filter(.iteration < 100) %>%
+    ## group_by(.chain, .iteration, S) %>%
+    ## summarize(pdf = sum(pdf)) %>%
+    ggplot(aes(S, pdf,
+               ## group = .iteration)) +
+               color = factor(k),
+               group = interaction(.iteration, .chain, k))) +
+    geom_line(alpha = 0.1) +
+    geom_vline(aes(xintercept = Spot)) +
+    scale_color_colorblind() +
+    facet_grid(Expiration ~ .) +
+    ## theme_economist_white() +
+    theme_tufte() +
+    theme(text = element_text(size = 12),
+          legend.position = "top") +
+    labs(x = "Stock price",
+         y = "Implied risk-neutral density",
+         color = "Component",
+         title = "S&P 500",
+         subtitle = paste("Option chain from", today(),
+                          sep = " "))
+
+ggsave("reports/risk_neutral.pdf")
+
+fits %>%
+    mutate(theta = map(fits,
+                       ~ .x %>%
+                           spread_draws(theta[k]))) %>%
+    unnest(theta) %>%
+    filter(K == 2 & k == 1) %>%
+    ggplot(aes(factor(Expiration), theta)) +
+    geom_tufteboxplot() +
+    theme_tufte() +
+    theme(text = element_text(size = 12)) +
+    labs(x = "Expiration date",
+         y = "Crash probability",
+         title = "S&P 500",
+         subtitle = paste("Option chain from", today(),
+                          sep = " ")) +
+    coord_cartesian(ylim = c(0, 0.5))
+
+ggsave("reports/crash_prob.pdf")
